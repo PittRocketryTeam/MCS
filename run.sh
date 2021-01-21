2b31f156fe701dd9890c946238cc73d59b3735a4
Patrick Roche <patrick@localhost.localdomain>
2021-01-20 22:00:08 -0500

    add some packets and logs dir

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --net=host --rm -e DISPLAY -e QT_X11_NO_MITSHM=1 -v $(pwd)/config:/cosmos/config ballaerospace/cosmos:4.4.2
+docker run --net=host --rm -e DISPLAY -e QT_X11_NO_MITSHM=1 -v $(pwd)/config:/cosmos/config  -v $(pwd)/logs:/cosmos/outputs/logs ballaerospace/cosmos:4.4.2
